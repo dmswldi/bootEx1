2fbdd25a373a853eaf04a965f251df8927245a2e
김은지 <eeeunzz20@mqnic.com>
2021-09-28 23:53:37 +0900

    Initial commit

---
+++ src/test/java/org/zerock/bootex1/BootEx1ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.bootex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads...");
+    }
+
+}
